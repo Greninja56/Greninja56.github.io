db57fd9166147479737fd196146eeac5b754cb39
Thomas Parameswaran <thpa8174@colorado.edu>
2025-09-17 16:52:53 -0600

    in class week 5

--- Class5/script.js
+++ Class5/script.js
@@ -1,10 +1,9 @@
-// Grab ALL buttons
 const expandBtns = document.querySelectorAll('.expand-btn');
 
 expandBtns.forEach(btn => {
   btn.addEventListener('click', () => {
-    const card = btn.closest('.card');         // find this card
-    const details = card.querySelector('.details'); // its details
+    const card = btn.closest('.card');
+    const details = card.querySelector('.details');
     const expanded = btn.getAttribute('aria-expanded') === 'true';
 
     btn.setAttribute('aria-expanded', !expanded);
